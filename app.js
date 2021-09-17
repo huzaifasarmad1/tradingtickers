cb744411173706a1442dd0c1139dcf28f7f73273
Huzaifa <huzaifa8300@gmail.com>
2021-09-17 21:41:45 +0500

    fefefe

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const hostname = '127.0.0.1';
-const port = process.env.PORT;
+const port = 4000;
 const nodemailer = require('nodemailer');
 const asyncHandler = require('express-async-handler')
 // var ema = require('exponential-moving-average');
@@ -20,11 +20,11 @@ const {
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'kaftabmemon@gmail.com', //add email from where you want to send the email
-    pass: 'burlfish' //add pass for that email
+    user: '', //add email from where you want to send the email
+    pass: '' //add pass for that email
   }
 });
-mongoose.connect('mongodb+srv://khurram:khurramaftab@cluster0.7lsze.mongodb.net/colorchangedb', { useNewUrlParser: true });
+mongoose.connect('Mongoose connection string here', { useNewUrlParser: true });
 app.use(cors())
 const asyncIntervals = [];
 const runAsyncInterval = async (cb, interval, intervalIndex) => {
@@ -93,8 +93,8 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 //run a job every middnight
-cron.schedule('0 8 * * *', () => {
-  console.log('running a task every midnight');
+cron.schedule('0 15 * * *', () => {
+  console.log('running a task every 4am');
   func();
 //   funma();
 //   funema();
@@ -103,8 +103,6 @@ cron.schedule('0 8 * * *', () => {
 //   funhullma();
 //   funtema();
 });
-
-var iterator = 0;
 let increased_tickers = [];
 console.log('upside func')
 // setIntervalAsync(
@@ -1729,8 +1727,8 @@ let funtema = async  (json2, json1, smoothe) => {
   // console.log(smoothe + 'smoothed')
   console.log('inside func')
   // here we need to remove the extra tickers  okay..let me make the list on excel yes you can give me an excel file same as you gave me before
-   data = ["GHSI", "GSV", "SYN", "TRX", "ACST", "ITP", "PTN", "NAK", "ADXS", "GTE", "ASRT", "NSPR", "TLGT", "BIOL", "CSCW", "GPL", "XPL", "CTRM", "CFMS", "OGEN", "KIQ", "NAKD", "DNN", "INUV", "NAOV", "MUX", "ADMP", "NBY", "TBLT", "URG", "HSTO", "SHIP", "DFFN", "UXIN", "AEZS", "TNXP", "AIKI", "MTNB", "BORR", "RGLS", "ONTX", "INPX", "ZSAN", "PTE", "SNDL", "MKD", "BXRX", "CHEK", "CIDM", "ISR", "ASM", "NOVN", "NVCN", "UAMY", "CEI", "MOTS", "HDSN", "ATIF", "POAI", "DYNT", "TXMD", "PULM", "LKCO", "NEPT", "BRQS", "LPCN", "AVGR", "NGD", "ITRM", "NXTD", "TGB", "NMTR", "GNUS", "CCO", "XSPA", "IBIO", "GSAT", "USWS", "VEON", "CLBS", "GERN", "TMBR", "PHUN", "ESGC", "ZOM", "HEPA", "GTT", "ACRX", "DGLY", "TYME", "JFU", "CTXR", "ATHX", "SINT", "CLSN", "JAGX", "CIG", "UEC", "QD", "LYG", "STRM", "TRVN", "CRBP", "BEST", "REI", "TTOO", "HOFV", "HOTH", "TOPS", "DHY", "METX", "NGL", "ABEO", "VTGN", "TGC", "TRCH", "TTI", "ABEV", "MICT", "NBEV", "LAIX", "VTVT", "EXPR", "CHS", "ATOS", "SESN", "BOXL", "LQDA", "IAG", "NAT", "MARK", "OGI", "INFI", "DHF", "KOS", "TELL", "GNW", "LGHL", "WTRH", "IDEX", "VISL", "LLNW", "WTI", "COMS", "CIK", "XXII", "VBIV", "KXIN", "RRD", "SENS", "AHT", "REPH", "SPPI", "QEP", "AKBA", "RIG", "AUUD", "QTT", "UGP", "NXE", "SAN", "TOUR", "MBIO", "DSS", "CPG", "OPTT", "TRXC", "ABUS", "HMY", "ENLC", "EVFM", "IVR", "MREO", "BRFS", "OBSV", "NOK", "JE", "CDEV", "AUY", "MFA", "SWN", "DPW", "BBD", "RIGL", "NYMT", "PAVM", "BTU", "XTNT", "TRVG", "TEF", "WATT", "BTG", "CRNT", "ETM", "BGCP", "OPK", "ITUB", "SRTS", "GGB", "FLDM", "AQMS", "AIV", "SOS", "ERF", "HLX", "ASMB", "TCDA", "AFI", "EFOI", "UUUU", "AREC", "ZIOP", "JG", "CNDT", "AMRX", "BBVA", "LIXT", "DHT", "LTRPA", "AFMD", "MNKD", "BKD", "SVM", "SIRI", "SID", "GLOG", "EXK", "CLNY", "CBAT", "CLVS", "SAND", "HMHC", "WIT", "WWR", "CERS", "SOLO", "KGC", "VET", "ALTO", "HL", "SBS", "CX", "ENBL", "MGI", "AMRN", "FRSX", "EBON", "HEXO", "KNDI", "AQB", "AGI", "VXRT", "AYRO", "CXW", "GEO", "TWO", "ETRN", "PSEC", "ZIXI", "PTEN", "CVE", "TV", "KALA", "GPRO", "FSM", "UWMC", "ASX", "ADT", "ET", "COTY", "MOGO", "GEL", "PBR", "ENDP", "AMC", "UAVS", "GFI", "FTI", "MBT", "EQX", "WIMI", "SILV", "NLY", "NG", "KOPN", "PAA", "PBI", "PAGP", "KODK", "DVAX", "ERJ", "AM", "BCS", "LOTZ", "AR", "CDE", "IRWD", "PGRE", "CGEN", "WPRT", "CLOV", "KMPH", "NPTN", "RRC", "SRNE", "GEVO", "PVG", "BNGO", "UMC", "INFN", "NNDM", "TWLVU", "CFII", "OR", "MACQU", "BB", "SBEAU", "DRH", "NRZ", "CRDF", "RUBY", "SLAMU", "CRON", "EGO", "PCG", "ACB", "AUVI", "HEC", "TEVA", "BCRX", "SRNGU", "ING", "AACQ", "AMCR", "OCGN", "SHLX", "ALUS", "MRO", "INO", "ZNGA", "SGMO", "NGAC", "CIM", "SDC", "AJAX", "SFTW", "F", "RTP", "EAF", "FNB", "ACIC", "UNIT", "RAAC", "GSAH", "ATNX", "MTG", "NYCB", "BDN", "DB", "FOLD", "LUMN", "RMO", "QRTEA", "GLUU", "ERIC", "GE", "CNX", "FRX", "AMX", "GOEV", "MAC", "CLNE", "SUNW", "SHO", "VG", "CLF", "ISBC", "HIMX", "ROOT", "BTWN", "IPOF", "AMRS", "KPTI", "NPA", "SM", "KAR", "CDXC", "HOL", "AUPH", "NBLX", "XL", "SSRM", "PBF", "APLE", "PRMW", "DOYU", "TWNK", "CS", "HPE", "COMM", "INSG", "VLDR", "SABR", "KMI", "VTRS", "MVIS", "CBD", "GTES", "MIK", "LU", "SY", "RLGY", "NOV", "HYLN", "CAN", "M", "BFT", "HBAN", "MDRX", "UBS", "CCJ", "SKT", "SLM", "MOMO", "GPK", "AGNC", "CLDR", "WKHS", "ACI", "VIAV", "FHN", "MUR", "STLA", "GMBL", "HST", "X", "IBN", "WES", "ABR", "KOSS", "TAK", "GT", "VALE", "FCEL", "DOC", "BPY", "INFY", "FLR", "VOD", "VST", "RLX", "YSG", "HBI", "GHVI", "EQT", "APHA", "PBCT", "AG", "ESI", "NKLA", "FLEX", "UA", "TGNA", "IGT", "KIM", "TROX", "SBSW", "BOX", "IPOE", "JBLU", "COG", "LTHM", "SVMK", "GOLD", "LAC", "CYTK", "VUZI", "EQNR", "BFLY", "HRB", "FEYE", "ORI", "RIDE", "CNP", "NLOK", "WISH", "BRX", "APA", "SU", "EB", "MFC", "AU", "DM", "KEY", "MAT", "OUT", "RDN", "WEN", "PS"];
-  // data = ["GHSI", "GPL", "GSV", "SYN", "TRX", "ACST", "ITP", "PTN", "NAK", "ADXS", "GTE", "ASRT", "NSPR", "TLGT", "BIOL", "CSCW", "GPL", "XPL"];
+   let data = ["GHSI", "GSV", "SYN", "TRX", "ACST", "ITP", "PTN", "NAK", "ADXS", "GTE", "ASRT", "NSPR", "TLGT", "BIOL", "CSCW", "GPL", "XPL", "CTRM", "CFMS", "OGEN", "KIQ", "NAKD", "DNN", "INUV", "NAOV", "MUX", "ADMP", "NBY", "TBLT", "URG", "HSTO", "SHIP", "DFFN", "UXIN", "AEZS", "TNXP", "AIKI", "MTNB", "BORR", "RGLS", "ONTX", "INPX", "ZSAN", "PTE", "SNDL", "MKD", "BXRX", "CHEK", "CIDM", "ISR", "ASM", "NOVN", "NVCN", "UAMY", "CEI", "MOTS", "HDSN", "ATIF", "POAI", "DYNT", "TXMD", "PULM", "LKCO", "NEPT", "BRQS", "LPCN", "AVGR", "NGD", "ITRM", "NXTD", "TGB", "NMTR", "GNUS", "CCO", "XSPA", "IBIO", "GSAT", "USWS", "VEON", "CLBS", "GERN", "TMBR", "PHUN", "ESGC", "ZOM", "HEPA", "GTT", "ACRX", "DGLY", "TYME", "JFU", "CTXR", "ATHX", "SINT", "CLSN", "JAGX", "CIG", "UEC", "QD", "LYG", "STRM", "TRVN", "CRBP", "BEST", "REI", "TTOO", "HOFV", "HOTH", "TOPS", "DHY", "METX", "NGL", "ABEO", "VTGN", "TGC", "TRCH", "TTI", "ABEV", "MICT", "NBEV", "LAIX", "VTVT", "EXPR", "CHS", "ATOS", "SESN", "BOXL", "LQDA", "IAG", "NAT", "MARK", "OGI", "INFI", "DHF", "KOS", "TELL", "GNW", "LGHL", "WTRH", "IDEX", "VISL", "LLNW", "WTI", "COMS", "CIK", "XXII", "VBIV", "KXIN", "RRD", "SENS", "AHT", "REPH", "SPPI", "QEP", "AKBA", "RIG", "AUUD", "QTT", "UGP", "NXE", "SAN", "TOUR", "MBIO", "DSS", "CPG", "OPTT", "TRXC", "ABUS", "HMY", "ENLC", "EVFM", "IVR", "MREO", "BRFS", "OBSV", "NOK", "JE", "CDEV", "AUY", "MFA", "SWN", "DPW", "BBD", "RIGL", "NYMT", "PAVM", "BTU", "XTNT", "TRVG", "TEF", "WATT", "BTG", "CRNT", "ETM", "BGCP", "OPK", "ITUB", "SRTS", "GGB", "FLDM", "AQMS", "AIV", "SOS", "ERF", "HLX", "ASMB", "TCDA", "AFI", "EFOI", "UUUU", "AREC", "ZIOP", "JG", "CNDT", "AMRX", "BBVA", "LIXT", "DHT", "LTRPA", "AFMD", "MNKD", "BKD", "SVM", "SIRI", "SID", "GLOG", "EXK", "CLNY", "CBAT", "CLVS", "SAND", "HMHC", "WIT", "WWR", "CERS", "SOLO", "KGC", "VET", "ALTO", "HL", "SBS", "CX", "ENBL", "MGI", "AMRN", "FRSX", "EBON", "HEXO", "KNDI", "AQB", "AGI", "VXRT", "AYRO", "CXW", "GEO", "TWO", "ETRN", "PSEC", "ZIXI", "PTEN", "CVE", "TV", "KALA", "GPRO", "FSM", "UWMC", "ASX", "ADT", "ET", "COTY", "MOGO", "GEL", "PBR", "ENDP", "AMC", "UAVS", "GFI", "FTI", "MBT", "EQX", "WIMI", "SILV", "NLY", "NG", "KOPN", "PAA", "PBI", "PAGP", "KODK", "DVAX", "ERJ", "AM", "BCS", "LOTZ", "AR", "CDE", "IRWD", "PGRE", "CGEN", "WPRT", "CLOV", "KMPH", "NPTN", "RRC", "SRNE", "GEVO", "PVG", "BNGO", "UMC", "INFN", "NNDM", "TWLVU", "CFII", "OR", "MACQU", "BB", "SBEAU", "DRH", "NRZ", "CRDF", "RUBY", "SLAMU", "CRON", "EGO", "PCG", "ACB", "AUVI", "HEC", "TEVA", "BCRX", "SRNGU", "ING", "AACQ", "AMCR", "OCGN", "SHLX", "ALUS", "MRO", "INO", "ZNGA", "SGMO", "NGAC", "CIM", "SDC", "AJAX", "SFTW", "F", "RTP", "EAF", "FNB", "ACIC", "UNIT", "RAAC", "GSAH", "ATNX", "MTG", "NYCB", "BDN", "DB", "FOLD", "LUMN", "RMO", "QRTEA", "GLUU", "ERIC", "GE", "CNX", "FRX", "AMX", "GOEV", "MAC", "CLNE", "SUNW", "SHO", "VG", "CLF", "ISBC", "HIMX", "ROOT", "BTWN", "IPOF", "AMRS", "KPTI", "NPA", "SM", "KAR", "CDXC", "HOL", "AUPH", "NBLX", "XL", "SSRM", "PBF", "APLE", "PRMW", "DOYU", "TWNK", "CS", "HPE", "COMM", "INSG", "VLDR", "SABR", "KMI", "VTRS", "MVIS", "CBD", "GTES", "MIK", "LU", "SY", "RLGY", "NOV", "HYLN", "CAN", "M", "BFT", "HBAN", "MDRX", "UBS", "CCJ", "SKT", "SLM", "MOMO", "GPK", "AGNC", "CLDR", "WKHS", "ACI", "VIAV", "FHN", "MUR", "STLA", "GMBL", "HST", "X", "IBN", "WES", "ABR", "KOSS", "TAK", "GT", "VALE", "FCEL", "DOC", "BPY", "INFY", "FLR", "VOD", "VST", "RLX", "YSG", "HBI", "GHVI", "EQT", "APHA", "PBCT", "AG", "ESI", "NKLA", "FLEX", "UA", "TGNA", "IGT", "KIM", "TROX", "SBSW", "BOX", "IPOE", "JBLU", "COG", "LTHM", "SVMK", "GOLD", "LAC", "CYTK", "VUZI", "EQNR", "BFLY", "HRB", "FEYE", "ORI", "RIDE", "CNP", "NLOK", "WISH", "BRX", "APA", "SU", "EB", "MFC", "AU", "DM", "KEY", "MAT", "OUT", "RDN", "WEN", "PS"];
+  // let data = ["GHSI", "GPL", "GSV", "SYN", "TRX", "ACST", "ITP"];
   let d = new Date();
   let todayDate = formatDate(d);
   let oldDate = formatDate(d.setDate(d.getDate() - 1500));
@@ -1738,18 +1736,18 @@ let funtema = async  (json2, json1, smoothe) => {
   // console.log(oldDate.length-size);
   console.log(oldDate + 'oldDate,')
   console.log(todayDate + 'todayDate')
-
+  let iterator = 0;
   // for(var smoothe=1;smoothe<=4;smoothe++){
   //  for (iterator=0;iterator<data.length;iterator++) {
   var interval = setAsyncInterval(async func2 => {
     console.log('inside set interval')
     var ticker = {};
-  
+    console.log('asdasdasd',iterator)
     try {
-      console.log('in try')
+      console.log('in try',`https://api.tiingo.com/tiingo/daily////${data[iterator]}/prices?token=`)
       let response1 = await fetch(
         //${data[iterator]}
-        `https://api.tiingo.com/tiingo/daily////${data[iterator]}/prices?token=e15237c196fb99408d604a9cce465d401a8c7b22`, {
+        `https://api.tiingo.com/tiingo/daily////${data[iterator]}/prices?token=`, {
         method: 'GET',
         headers: {
           Accept: 'application/json',
@@ -1761,10 +1759,10 @@ let funtema = async  (json2, json1, smoothe) => {
       // ticker = { date: json1[0].date, ticker: data[iterator], closed_price: json1[0].close, Volume:json1[0].volume }
       console.log(json1)
       if (json1.length > 0) {
-        console.log('inside fetch if')
+        console.log('inside fetch if',`https://api.tiingo.com/tiingo/daily////${data[iterator]}/prices?startDate=${oldDate}&endDate=${json1[0].date}&token=`)
         //second one
         let response2 = await fetch(
-          `https://api.tiingo.com/tiingo/daily////${data[iterator]}/prices?startDate=${oldDate}&endDate=${json1[0].date}&token=e15237c196fb99408d604a9cce465d401a8c7b22`, {
+          `https://api.tiingo.com/tiingo/daily////${data[iterator]}/prices?startDate=${oldDate}&endDate=${json1[0].date}&token=`, {
           method: 'GET',
           headers: {
             Accept: 'application/json',
@@ -1866,7 +1864,7 @@ let funtema = async  (json2, json1, smoothe) => {
     // setup e-mail data with unicode symbols
     var mailOptions = {
       from: '"Tickers changed ?" <no-reply@tickers.com>', // sender address
-      to: 'memonkhurramaftab@gmail.com', // list of receivers( , separated here)
+      to: '', // list of receivers( , separated here)
       subject: 'Here is the daily report of tickers that are changed', // Subject line
       html: email_html // html body
   
@@ -1876,6 +1874,7 @@ let funtema = async  (json2, json1, smoothe) => {
       if (error) {
         return console.log(error);
       }
+      console.log(mailOptions)
       console.log('Message sent: ' + info.response);
   
     });
@@ -1885,71 +1884,9 @@ let funtema = async  (json2, json1, smoothe) => {
 // 
     // return statusarray;
   
-  }, 10000);
+  }, 23000);
   // };
   }
-  // let email_html = `<!DOCTYPE html>
-  //   <html lang="en">
-  //   <head>
-  //     <title>Ticker</title>
-  //     <meta charset="utf-8">
-  //     <meta name="viewport" content="width=device-width, initial-scale=1">
-  //     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  //     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  //     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  //     <script src="https://www.w3schools.com/lib/w3.js"></script>
-     
-  //   </head>
-  //   <body  onload="get_fn()">
-    
-  //   <div class="container">
-      
-  //       <table class="table">
-  //           <thead class="thead-dark">
-  //             <tr>
-  //               <th scope="col">Date</th>
-  //               <th scope="col">Ticker</th>
-  //               <th scope="col">Status</th>
-  //               <th scope="col">Closed Price</th>
-  //               <th scope="col">Average_type</th>
-  //               <th scope="col">Smoothe</th>
-  //             </tr>
-  //           </thead>
-  //           <tbody>`;
-  // for (const ticker of increased_tickers) {
-  //   email_html += `<tr>
-  //             <th scope="row">${ticker.date}</th>
-  //             <td>${ticker.ticker}</td>
-  //             <td>${ticker.status}</td>
-  //             <td>${ticker.closed_price}</td>
-  //             <td>${ticker.average_type}</td>
-  //             <td>${ticker.smoothe}</td>
-  //           </tr>`
-  // }
-
-  // email_html += `</tbody>
-  //         </table>
-  //         </div>  
-  //   </body>
-  //   </html>`
-  // //Send email here
-  // // setup e-mail data with unicode symbols
-  // var mailOptions = {
-  //   from: '"Tickers ?" <no-reply@tickers.com>', // sender address
-  //   to: '', // list of receivers( , separated here)
-  //   subject: 'Here is the daily report of tickers increased', // Subject line
-  //   html: email_html // html body
-
-  // };
-  // // send mail with defined transport object
-  // transporter.sendMail(mailOptions, function (error, info) {
-  //   if (error) {
-  //     return console.log(error);
-  //   }
-  //   console.log('Message sent: ' + info.response);
-
-  // });
-  //  }
 
 func();
 // }, 10000
